c6ae4d5b6c528433752da42f7609f0423e2c0e5d
Carlos Córcoles Toledo <carloscorcolest@gmail.com>
2015-05-01 11:46:43 +0200

    Updated offsets for 2.0.7.1154797

--- eso-framework/eso-framework/Patchables/Offsets.h
+++ eso-framework/eso-framework/Patchables/Offsets.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <windows.h>
 
-// Version : eso.live.2.0.6.1151008
+// Version : eso.live.2.0.7.1154797
 // All the pointers are 0x400000 based.
 
 class Offsets
@@ -11,7 +11,7 @@ public:
 	
 	enum class ZoRenderManager
 	{
-		s_pZoRenderManager = 0x1B30F60 - 0x400000,
+		s_pZoRenderManager = 0x1B31F58 - 0x400000,
 		m_renderDeviceD3D9 = 0x4,											// From : ZoRenderManager::Present
 		m_renderApiType = 0x84,												// From : ZosClient::Initialize
 	};
@@ -23,35 +23,35 @@ public:
 
 	enum class ClientCoreHandles
 	{
-		g_pClientCore = 0x198DC5C - 0x400000,
+		g_pClientCore = 0x198EC5C - 0x400000,
 		m_unitList = 0xD8,													// From : CancelCast
-		GetUnitListAddress = 0x005A5B30 - 0x400000,
+		GetUnitListAddress = 0x005A5EF0 - 0x400000,
 	};
 	
 	enum class UnitList
 	{
-		GetFirstUnit = 0x00A017F0 - 0x400000,									// UnitList::GetFirstUnit
-		GetNextUnit = 0x00A02990 - 0x400000,									// UnitList::GetNextUnit
-		GetIterUnit = 0x00A01830 - 0x400000,									// UnitList::GetIterUnit
+		GetFirstUnit = 0x00A02190 - 0x400000,									// UnitList::GetFirstUnit
+		GetNextUnit = 0x00A03330 - 0x400000,									// UnitList::GetNextUnit
+		GetIterUnit = 0x00A021D0 - 0x400000,									// UnitList::GetIterUnit
 		m_playerUnit = 0x50,												// From : UnitList::GetPlayerUnit
 	};
 	
 	enum class Unit
 	{
-		GetName = 0x009F88D0 - 0x400000,										// Unit::GetName
-		GetPosition = 0x009F0CC0 - 0x400000,									// Unit::GetPosition
-		GetRenderHeading = 0x009F3380 - 0x400000,								// Unit::GetRenderHeading
+		GetName = 0x009F9270 - 0x400000,										// Unit::GetName
+		GetPosition = 0x009F1660 - 0x400000,									// Unit::GetPosition
+		GetRenderHeading = 0x009F3D20 - 0x400000,								// Unit::GetRenderHeading
 		m_type = 0x94,														// From : Unit::IsKillable
 	};
 	
 	enum class CameraManager
 	{
-		s_pCameraManager = 0x198CC68 - 0x400000,
+		s_pCameraManager = 0x198DC68 - 0x400000,
 	};
 	
 	enum class ClientWorld
 	{
-		g_pClientWorld = 0x198D6DC - 0x400000,
+		g_pClientWorld = 0x198E6DC - 0x400000,
 		m_camera = 0x5E0,													// From : UpdateZoRenderData
 	};
 	
@@ -63,7 +63,7 @@ public:
 	
 	enum class WorldPosition
 	{
-		ToRenderPosition = 0x0089F700 - 0x400000,
+		ToRenderPosition = 0x0089FFE0 - 0x400000,
 	};
 
 };
